6e2047d6f0f28a1d90b8827e16179799007318b1
GnoGcia <142554475+GnoGcia@users.noreply.github.com>
2024-02-21 16:03:09 -0800

    Add files via upload

---
+++ CSC_450-1_MOD2_CTA.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Declare three string variables
+    // Two to store user input
+    // One to store the concatenated result
+    string input1;
+    string input2;
+    string result;
+
+    // Prompt for and record user input
+    cout << "String 1: ";
+    cin >> input1;
+    cout << "String 2: ";
+    cin >> input2;
+
+    // Concatenate the two inputted strings
+    result = input1 + input2;
+
+    // Output the concatenated strings
+    cout << "Concatenated Strings: " + result << endl;
+
+    // Prompt for and record user input
+    cout << "String 1: ";
+    cin >> input1;
+    cout << "String 2: ";
+    cin >> input2;
+
+    // Concatenate the two inputted strings
+    result = input1 + input2;
+
+    // Output the concatenated strings
+    cout << "Concatenated Strings: " + result << endl;
+
+    // Prompt for and record user input
+    cout << "String 1: ";
+    cin >> input1;
+    cout << "String 2: ";
+    cin >> input2;
+
+    // Concatenate the two inputted strings
+    result = input1 + input2;
+
+    // Output the concatenated strings
+    cout << "Concatenated Strings: " + result << endl;
+
+    return 0;
+}
